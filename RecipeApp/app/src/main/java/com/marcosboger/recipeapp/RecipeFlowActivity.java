f7283b55672cf64f42f2923a1b94827932526206
Marcos Boger <marcos.boger@usp.br>
2020-02-12 15:48:24 +0100

    Video working (not good)
    Video working, but not in a very good way. Using google drive is slowing things down

--- RecipeApp/app/src/main/java/com/marcosboger/recipeapp/RecipeFlowActivity.java
+++ RecipeApp/app/src/main/java/com/marcosboger/recipeapp/RecipeFlowActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -35,6 +38,8 @@ import javax.xml.parsers.SAXParserFactory;
 
 public class RecipeFlowActivity extends AppCompatActivity {
 
+    private Bitmap playButtonBitmap;
+    private boolean paused = true;
     private static int numberSteps = -1;
     private static int j = 0;
     private static Bitmap[] bitmapArray = new Bitmap[20];
@@ -45,30 +50,27 @@ public class RecipeFlowActivity extends AppCompatActivity {
     private String[] recipeVideo = new String[20];
     private String[] recipeIsFinal = new String[20];
     TextView text;
-    Button lastStep, nextStep;
+    Button lastStepButton, nextStepButton, startButton;
     ImageView image;
     VideoView video;
     DataSnapshot steps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String recipeName;
+        final String recipeName;
         DatabaseReference mDatabase;
-        String vidAddress = "http://example.com/examplevideo.mp4";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_flow);
         recipeName = getIntent().getStringExtra("recipe_name");
-        lastStep = findViewById(R.id.last_step_button);
+        lastStepButton = findViewById(R.id.last_step_button);
         text = findViewById(R.id.recipe_text);
-        nextStep = findViewById(R.id.next_step_button);
+        nextStepButton = findViewById(R.id.next_step_button);
+        startButton = findViewById(R.id.start_video_button);
         image = findViewById(R.id.recipe_image);
         video = findViewById(R.id.recipe_video);
-        lastStep.setEnabled(false);
-        nextStep.setEnabled(false);
-        //Uri vidUri = Uri.parse(vidAddress);
-        //video.setVideoURI(vidUri);
-        //video.requestFocus();
-        //video.start();
+        lastStepButton.setEnabled(false);
+        nextStepButton.setEnabled(false);
+        playButtonBitmap = getBitmap(getResources().getDrawable(R.drawable.ic_play_arrow_24px));
 
         mDatabase = FirebaseDatabase.getInstance().getReference("recipes").child(recipeName).child("steps");
 
@@ -83,11 +85,17 @@ public class RecipeFlowActivity extends AppCompatActivity {
                     recipeVideo[i] = steps.child(Integer.toString(i)).child("video").getValue(String.class);
                     recipeIsFinal[i] = steps.child(Integer.toString(i)).child("final").getValue(String.class);
                     new RetrieveFeedTask().execute(recipeImage[i]);
-                    if(j == numberSteps){
-                        nextStep.setEnabled(true);
-                        image.setImageBitmap(bitmapArray[0]);
-                        text.setText(recipeText[0]);
+                }
+                if(j == numberSteps){
+                    nextStepButton.setEnabled(true);
+                    image.setImageBitmap(bitmapArray[0]);
+                    text.setText(recipeText[0]);
+                    if(recipeVideo[0].equals("none")){
+                        video.setVisibility(View.GONE);
+                        startButton.setVisibility(View.GONE);
                     }
+                    else
+                        video.setVideoURI(Uri.parse(recipeVideo[0]));
                 }
             }
 
@@ -98,30 +106,81 @@ public class RecipeFlowActivity extends AppCompatActivity {
         };
         mDatabase.addListenerForSingleValueEvent(textListener);
         if(j == numberSteps){
-            nextStep.setEnabled(true);
+            nextStepButton.setEnabled(true);
             image.setImageBitmap(bitmapArray[0]);
             text.setText(recipeText[0]);
+            if(recipeVideo[0].equals("none")){
+                video.setVisibility(View.GONE);
+                startButton.setVisibility(View.GONE);
+            }
+            else
+                video.setVideoURI(Uri.parse(recipeVideo[0]));
+        }
+    }
+
+    public void onStartVideoClicked(View view){
+        Log.d("RecipeFlowActivity",Boolean.toString(paused));
+        if(paused){
+            image.setImageBitmap(null);
+            video.start();
+            paused = false;
+        }
+        else{
+            video.pause();
+            image.setImageBitmap(playButtonBitmap);
+            paused = true;
         }
     }
 
     public void onNextStepClicked(View view){
         if(step == 0)
-            lastStep.setEnabled(true);
+            lastStepButton.setEnabled(true);
         step++;
+        paused = true;
         text.setText(recipeText[step]);
         image.setImageBitmap(bitmapArray[step]);
+        if(recipeVideo[step].equals("none")){
+            video.setVisibility(View.GONE);
+            startButton.setVisibility(View.GONE);
+        }
+        else{
+            startButton.setVisibility(View.VISIBLE);
+            video.setVideoURI(Uri.parse(recipeVideo[step]));
+            video.setVisibility(View.VISIBLE);
+            video.pause();
+        }
         if(recipeIsFinal[step].equals("true"))
-            nextStep.setEnabled(false);
+            nextStepButton.setEnabled(false);
     }
 
     public void onLastStepClicked(View view){
         if(recipeIsFinal[step].equals("true"))
-            nextStep.setEnabled(true);
+            nextStepButton.setEnabled(true);
         step--;
+        paused = true;
         text.setText(recipeText[step]);
         image.setImageBitmap(bitmapArray[step]);
+        if(recipeVideo[step].equals("none")){
+            video.setVisibility(View.GONE);
+            startButton.setVisibility(View.GONE);
+        }
+        else{
+            startButton.setVisibility(View.VISIBLE);
+            video.setVideoURI(Uri.parse(recipeVideo[step]));
+            video.setVisibility(View.VISIBLE);
+            video.pause();
+        }
         if(step == 0)
-            lastStep.setEnabled(false);
+            lastStepButton.setEnabled(false);
+    }
+
+    private Bitmap getBitmap(Drawable vectorDrawable) {
+        Bitmap bitmap = Bitmap.createBitmap(200,
+                200, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vectorDrawable.draw(canvas);
+        return bitmap;
     }
 
 
@@ -131,7 +190,7 @@ public class RecipeFlowActivity extends AppCompatActivity {
 
         protected Bitmap doInBackground(String... recipeImage) {
             if (recipeImage[0].equals("none"))
-                return null;
+                return playButtonBitmap;
             URL url = null;
             Bitmap bmp = null;
             try {
@@ -152,9 +211,15 @@ public class RecipeFlowActivity extends AppCompatActivity {
             bitmapArray[j] = bmp;
             j++;
             if(j == numberSteps){
-                nextStep.setEnabled(true);
+                nextStepButton.setEnabled(true);
                 image.setImageBitmap(bitmapArray[0]);
                 text.setText(recipeText[0]);
+                if(recipeVideo[0].equals("none")){
+                    video.setVisibility(View.GONE);
+                    startButton.setVisibility(View.GONE);
+                }
+                else
+                    video.setVideoURI(Uri.parse(recipeVideo[0]));
             }
         }
     }
